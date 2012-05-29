4dd91a776a22afdd566f891e80b77956e320ba4b
Sebastian Stockebrand <sebastian.stockebrand@gmail.com>
2012-05-29 16:03:52 -0400

    Finish sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 SampleApp::Application.routes.draw do
 
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
 
   #get "static_pages/home"
   root to: 'static_pages#home'
 
   #get "users/new"
   match '/signup', to: 'users#new'
-
+  match '/signin', to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy', via: :delete
 
   #get "static_pages/help"
   match '/help', to: 'static_pages#help'
