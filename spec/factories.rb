6b1574793b0a7b95eeb524c25bd0e2ab91659f9f
Sebastian Stockebrand <sebastian.stockebrand@gmail.com>
2012-05-30 16:08:59 -0400

    Finish user edit, update, index and destroy actions

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,17 @@
 FactoryGirl.define  do
 	factory :user do
-		name "Sebastian Stockebrand"
-		email "seba@example.com"
+		# name "Sebastian Stockebrand"
+		# email "seba@example.com"
+		# password "foobar"
+		# password_confirmation "foobar"
+
+		sequence(:name) { |n| "Person #{n}" }
+		sequence(:email) { |n| "person_#{n}@example.com"}
 		password "foobar"
 		password_confirmation "foobar"
+
+		factory :admin do
+			admin true
+		end
 	end
 end
